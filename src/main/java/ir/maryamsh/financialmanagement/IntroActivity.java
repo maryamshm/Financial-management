9e6e8e427868e44d8876aa67c25e1e543bae07f4
maryam-shahmohammadi <mar.shahmohammadi@gmail.com>
2021-05-25 17:48:05 +0430

    design layout

---
+++ src/main/java/ir/maryamsh/financialmanagement/IntroActivity.java
@@ -0,0 +1,15 @@
+package ir.maryamsh.financialmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
---
+++ src/main/java/ir/maryamsh/financialmanagement/MainActivity.java
@@ -0,0 +1,18 @@
+package ir.maryamsh.financialmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button BtnLogin,BtnSingin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
