8d6b9bb2d7e95103f83b6d1f8ed90bae430ecffd
maryam-shahmohammadi <mar.shahmohammadi@gmail.com>
2021-05-26 14:42:10 +0430

    intro layout

---
+++ src/main/java/ir/maryamsh/financialmanagement/HomeActivity.java
@@ -0,0 +1,14 @@
+package ir.maryamsh.financialmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.items_intro);
+    }
+}
--- src/main/java/ir/maryamsh/financialmanagement/MainActivity.java
+++ src/main/java/ir/maryamsh/financialmanagement/MainActivity.java
@@ -2,8 +2,10 @@ package ir.maryamsh.financialmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,7 +14,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.items_intro);
+        init();
+//        BtnSingin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this,SingupActivity.class));
+//            }
+//        });
+//        BtnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+//            }
+//        });
+    }
 
+    private void init() {
+      BtnLogin=findViewById(R.id.BtnLogin);
+      BtnSingin=findViewById(R.id.BtnSingin);
     }
 }
